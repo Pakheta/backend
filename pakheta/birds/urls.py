078bdb8371df91943839fb0b2dcf0c17bb9432b9
gaurovgiri <gaurovgiri@gmail.com>
2025-01-05 02:41:14 +0545

    add data for bird species 1

--- pakheta/birds/urls.py
+++ pakheta/birds/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.BirdListView.as_view()),
     path("assets/<int:bird_id>/", views.BirdsetView.as_view()),
-    path("<int:bird_id>/", views.BirdView.as_view())
+    path("<int:bird_id>/", views.BirdView.as_view()),
+    path("predict/", views.BirdPredictionView.as_view()),
 ]
--- pakheta/birds/views.py
+++ pakheta/birds/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Bird, Birdset
 from .serializers import BirdSerializer, BirdsetSerializer
+import requests
 
 class BirdListView(APIView):
     def get(self, request):
@@ -23,3 +24,22 @@ class BirdsetView(APIView):
         birds = Birdset.objects.filter(bird=bird)
         serializer = BirdsetSerializer(birds, many=True)
         return Response(serializer.data)
+
+class BirdPredictionView(APIView):
+    def post(self, request):
+        audio_file = request.FILES.get('audio')
+        
+        if not audio_file:
+            return Response({"error": "No audio file provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+        response = requests.post(
+            'https://mryeti-featherfindapi.hf.space/predict/',
+            files={'audio_file': audio_file}
+        )
+
+        if response.status_code != 200:
+            return Response({"error": "Failed to get prediction from external API"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        prediction = response.json()
+
+        return Response(prediction, status=status.HTTP_200_OK)
