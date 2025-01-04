4ce083a77c8b8f2417b458c542122ee98cbec609
Roshan Pandey <rpandox@gmail.com>
2025-01-04 09:25:58 +0545

    Callback

--- pakheta/callback/views.py
+++ pakheta/callback/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def my_view(request):
-    return render(request,'authlanding.html', context)
+    return render(request,'authlanding.html')
